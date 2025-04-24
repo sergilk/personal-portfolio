9f059c0e7985e34779e850412e0a0f753ffd3b5d
sergilk <liashko.sergi@gmail.com>
2025-04-24 03:56:24 +0300

    Updating portfolio links

--- src/data/data.js
+++ src/data/data.js
@@ -3,8 +3,8 @@ export const projects = [
     id: 1,
     name: "Portfolio",
     img: "./assets/projects/portfolio.webp",
-    repoLink: "#",
-    demoLink: "#",
+    repoLink: "https://github.com/sergilk/personal-portfolio",
+    demoLink: "https://sergilk.github.io/personal-portfolio/",
     descr:
       "A modern, minimalistic personal portfolio built to showcase completed projects and skills. The design is responsive, and it supports full customization, allowing you to easily adjust the style to suit your needs. The primary goal of the project is to practice building applications using modern technologies such as TailwindCSS v4 and React.js",
     techs: ["React.js", "TailwindCSS", "Vite"],
